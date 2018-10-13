326f2308ffd26a5d6964d4a803bba9527951b9a8
ergouzi <1763354042@qq.com>
2018-10-13 22:26:42 +0800

    测试数据库返回json结果

--- ergouzi/JobData/urls.py
+++ ergouzi/JobData/urls.py
@@ -8,5 +8,9 @@ urlpatterns =[
     path('data',views.data,name = 'data'),
     path('page',views.page,name = 'page'),
     path('post',views.post,name = 'post'),
+    path('get_avg_salary',views.get_avg_salary,name = 'get_avg_salary'),
+    path('get_city_list',views.get_city_list,name = 'get_city_list'),
+    path('get_city_salary',views.get_city_salary,name = 'get_city_salary'),
+    path('get_salary_range',views.get_salary_range,name = 'get_salary_range'),
     path('Recommend',views.Recommend,name = 'Recommend'),
 ]
--- ergouzi/JobData/views.py
+++ ergouzi/JobData/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Salaryrange
+from .models import AvgSalary
+from .models import Citylist
+from .models import Citysalary
+from django.http import JsonResponse
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
@@ -16,19 +20,36 @@ def data(request):
 
 def page(request):
     keyword = request.GET.get('keyWord')
-    list = Salaryrange.objects.get(id = 1)
-    for key in list:
-        print(key)
-
-
-    contex = {
-        # "list":list[0],
-        "keyWord":keyword
-    }
-    return render(request,'JobData/page.html',{contex})
+    print(keyword+'123')
+    rep = render(request,'JobData/page.html')
+    rep.set_cookie("keyWord",keyword)
+    return rep
 
 def post(request):
     return render(request,'JobData/post.html')
 
 def Recommend(request):
     return render(request,'JobData/Recommend.html')
+
+def get_avg_salary(request):
+    keyword = request.COOKIES["keyWord"]
+    avg = AvgSalary.objects.filter(keyword = keyword)
+    avg_dic = {}
+    avg_dic["keyWord"] = avg[0].keyword
+    avg_dic["avgsalary"] = avg[0].avgsalary
+    return JsonResponse(avg_dic)
+
+def get_city_list(request):
+    keyword = request.COOKIES["keyWord"]
+    avg = Citylist.objects.filter(keyword = keyword)
+    return JsonResponse(avg[0])
+
+def get_city_salary(request):
+    keyword = request.COOKIES["keyWord"]
+    avg = Citysalary.objects.filter(keyword = keyword)
+    return JsonResponse(avg)
+
+def get_salary_range(request):
+    keyword = request.COOKIES["keyWord"]
+    avg = Salaryrange.objects.filter(keyword = keyword)
+    return JsonResponse(avg)
