5ecc87fa6826284853175ef132cd5c055b415a8b
ergouzi <1763354042@qq.com>
2018-10-14 12:06:23 +0800

    测试画图组件，在逐步修改

--- ergouzi/JobData/views.py
+++ ergouzi/JobData/views.py
@@ -20,7 +20,6 @@ def data(request):
 
 def page(request):
     keyword = request.GET.get('keyWord')
-    print(keyword+'123')
     rep = render(request,'JobData/page.html')
     rep.set_cookie("keyWord",keyword)
     return rep
@@ -32,24 +31,47 @@ def Recommend(request):
     return render(request,'JobData/Recommend.html')
 
 def get_avg_salary(request):
-    keyword = request.COOKIES["keyWord"]
-    avg = AvgSalary.objects.filter(keyword = keyword)
-    avg_dic = {}
-    avg_dic["keyWord"] = avg[0].keyword
-    avg_dic["avgsalary"] = avg[0].avgsalary
-    return JsonResponse(avg_dic)
+    avgSalaryList = []
+    avg_salary = AvgSalary.objects.all()
+    for item in avg_salary:
+        avg_salary_dic = {}
+        avg_salary_dic["keyWord"] = item.keyword
+        avg_salary_dic["avgSalary"] = item.avgsalary
+        avgSalaryList.append(avg_salary_dic)
+    return JsonResponse(avgSalaryList,safe=False)
 
 def get_city_list(request):
+    cityListList=[]
     keyword = request.COOKIES["keyWord"]
-    avg = Citylist.objects.filter(keyword = keyword)
-    return JsonResponse(avg[0])
+    city_list = Citylist.objects.filter(keyword = keyword).order_by('value')[0:10]
+    for item in city_list:
+        cityListDic = {}
+        cityListDic["keyWord"] = item.keyword
+        cityListDic["city"] = item.city
+        cityListDic["value"] = item.value
+        cityListList.append(cityListDic)
+    return JsonResponse(cityListList,safe=False)
 
 def get_city_salary(request):
+    citySalaryList = []
     keyword = request.COOKIES["keyWord"]
-    avg = Citysalary.objects.filter(keyword = keyword)
-    return JsonResponse(avg)
+    citySalary = Citysalary.objects.filter(keyword = keyword)
+    for item in citySalary:
+        citySalaryDic = {}
+        citySalaryDic["keyWord"] = item.keyword
+        citySalaryDic["city"] = item.city
+        citySalaryDic["salary"] = item.salary
+        citySalaryList.append(citySalaryDic)
+    return JsonResponse(citySalaryList,safe=False)
 
 def get_salary_range(request):
+    salaryRangeList = []
     keyword = request.COOKIES["keyWord"]
-    avg = Salaryrange.objects.filter(keyword = keyword)
-    return JsonResponse(avg)
+    salary_range = Salaryrange.objects.filter(keyword = keyword)
+    for item  in salary_range:
+        salaryRangeDic = {}
+        salaryRangeDic["keyWord"] = item.keyword
+        salaryRangeDic["range"] = item.range
+        salaryRangeDic["num"] = item.num
+        salaryRangeList.append(salaryRangeDic)
+    return JsonResponse(salaryRangeList,safe=False)
